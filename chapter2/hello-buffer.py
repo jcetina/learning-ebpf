0678aea97867e9f6a964c3efe03ee83f7a19263a
Ubuntu <jcetina@ebpf.kwdkwgd3gxlefituouixwxwp0g.xx.internal.cloudapp.net>
2024-12-07 00:18:31 +0000

    chapter 1 modifications

--- chapter2/hello-buffer.py
+++ chapter2/hello-buffer.py
@@ -13,11 +13,17 @@ struct data_t {
  
 int hello(void *ctx) {
    struct data_t data = {}; 
-   char message[12] = "Hello World";
- 
+   char even[12] = "Hello Werld";
+   char odd[12] = "Hello World";
+   char *message = even;
+
    data.pid = bpf_get_current_pid_tgid() >> 32;
    data.uid = bpf_get_current_uid_gid() & 0xFFFFFFFF;
    
+   if (data.uid % 2) {
+       message = odd;
+    }
+
    bpf_get_current_comm(&data.command, sizeof(data.command));
    bpf_probe_read_kernel(&data.message, sizeof(data.message), message); 
  
--- chapter2/hello-map-count-syscalls.py
+++ chapter2/hello-map-count-syscalls.py
@@ -2,7 +2,7 @@
 from bcc import BPF
 from time import sleep
 
-program = r"""
+_program = r"""
 BPF_HASH(counter_table);
 
 int hello(void *ctx) {
@@ -19,15 +19,16 @@ int hello(void *ctx) {
    counter_table.update(&uid, &counter);
    return 0;
 }
+"""
 
-int openat(void *ctx) {
+program = r"""
+BPF_HASH(counter_table);
+
+RAW_TRACEPOINT_PROBE(sys_enter) {
    u64 uid;
    u64 counter = 0;
    u64 *p;
 
-   char command[16];
-
-   bpf_get_current_comm(&command, sizeof(command));
    uid = bpf_get_current_uid_gid() & 0xFFFFFFFF;
    p = counter_table.lookup(&uid);
    if (p != 0) {
@@ -35,26 +36,16 @@ int openat(void *ctx) {
    }
    counter++;
    counter_table.update(&uid, &counter);
-   bpf_trace_printk("openat");
    return 0;
 }
 """
 
-# bpf_trace_printk("%s", sizeof(command), command);
-
 b = BPF(text=program)
-syscall = b.get_syscall_fnname("execve")
-openat = b.get_syscall_fnname("openat")
-write = b.get_syscall_fnname("write")
-b.attach_kprobe(event=syscall, fn_name="hello")
-b.attach_kprobe(event=openat, fn_name="openat")
-b.attach_kprobe(event=write, fn_name="hello")
 
 # Attach to a tracepoint that gets hit for all syscalls 
 # b.attach_raw_tracepoint(tp="sys_enter", fn_name="hello")
 
 while True:
-    b.trace_print()
     sleep(2)
     s = ""
     for k,v in b["counter_table"].items():
